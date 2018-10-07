8f94fa971297137c85558270e903fd37323f55d1
vivekpatel99 <vivpatel99@gmail.com>
2018-10-07 14:34:06 +0200

    In define.py
     - set up after buffer test

--- definition/define.py
+++ definition/define.py
@@ -2,9 +2,9 @@
 
 """
 
-import numpy as np
+import os
 import mmap
-import struct
+
 
 VDMA_MAP_SIZE = 0x100  # UIO - VDMA
 VDMA_ADDR = 0x43C00000
@@ -22,7 +22,7 @@ HORIZ_PIXELS_SMALL = 640
 VERT_LINES_SMALL = 480
 N_BUFFERS = 2
 
-print('hello')
+
 
 
 
@@ -31,17 +31,13 @@ def platform_init():
 
     all_disp_address = HORIZONTAL_PIXELS * VERTICAL_LINES * PIXEL_NUM_OF_BYTES
     all_disp_small = HORIZ_PIXELS_SMALL * VERT_LINES_SMALL * PIXEL_NUM_OF_BYTES
-   
-    # 1st frame buffer mapping
-    fd_frbuf = open("/dev/fb0", 'wb+', buffering=0)
-   # print(np.fromfile(fd_frbuf, dtype=np.uint32))
-    print(struct.unpack('i', fd_frbuf.read(4))[0]) 
-   #fd_frbuf.flush()
-   # if fd_frbuf.read() < 1:
-    #    print("invalid fb0 device file \n")
+
+    # frame buffer mapping
+    fd_frbuf =  open("/dev/fb0", "r+")
+    if fd_frbuf < 1:
+        print("invalid fb0 device file \n")
 
     # mmap.mmap(fileno, length[, flags[, prot[, access[, offset]]]])
-   # ptr_frbuf = mmap.mmap(fileno=fd_frbuf.fileno(), length=all_disp_address,access=mmap.MAP_SHARED, prot=mmap.PROT_READ|mmap.PROT_WRITE,  offset=0)
-   # print("fb0 has allocated memory address {:#08x}".format(ptr_frbuf))
+    ptr_frbuf = mmap.mmap(fileno=fd_frbuf.fileno(), length=all_disp_address,access=mmap.MAP_SHARED, prot=mmap.PROT_READ|mmap.PROT_WRITE,  offset=0)
+    print("fb0 has allocated memory address {:#08x}".format(ptr_frbuf))
     fd_frbuf.close()
-platform_init()
