540f5a0a4b8d51f6b7515658759f7c90d87c71f3
vivekpatel99 <vivpatel99@gmail.com>
2018-10-14 14:08:27 +0000

    nothing special

--- zybo_7k_object_tracking/main.py
+++ zybo_7k_object_tracking/main.py
@@ -10,43 +10,50 @@ ptr_frbuf, ptr_frbuf_2, ptr_frbuf_3, ptr_frbuf_4, ptr_vdma, ptr_vdma_2, ptr_vdma
 """
 import sys
 import cv2
-import ctypes.util
-import ctypes
 import time
-from definition import define
+import  os
 import pygame
 
-# from PyQt5.QtWidgets import QApplication
-#
+from PyQt5.QtWidgets import QApplication
+
+
+
+from definition import define
+# from lib.display import display as dsp
 # from lib._Time import timecount
 # from lib.vision.vision import Vision
 # from lib._logger import logging
 # from gui import gui
-import  os
+
+
 
 def main():
     """
     """
     fram_bfs, vdma_bfs = define.platform_init()
 
-    # vid = Vision(cam_num=0)
-    # vid.is_camera_connected()
+    print(vdma_bfs)
+
+    # os.environ["SDL_FBDEV"] = vdma_bfs.vdma_buf_3
     cap = cv2.VideoCapture(0)
-    time.sleep(5)
-    out = cv2.VideoWriter("out.avi", cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 25, (640,480))
-    os.putenv(fram_bfs.fd_frbuf_2)
-    pygame.display.init()
+    # time.sleep(5)
+
+
     while True:
         # ret, frame = vid.get_video()
         #vid.display('img', frame)
         ret, frame = cap.read()
+        # HORIZ_PIXELS_SMALL = 640
+        # VERT_LINES_SMALL = 480
+        resized_frame = cv2.resize(frame,(define.HORIZ_PIXELS_SMALL, define.VERT_LINES_SMALL))
 
+        fram_bfs.fd_frbuf_3.write(str(frame))
 
 
         if cv2.waitKey(300) & 0xFF == ord("q"):
             break
 
-    # vid.video_cleanUp()
+#     # vid.video_cleanUp()
 
 
 if __name__ == '__main__':
